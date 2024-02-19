e7252aa7913967875ac1b9a6be5b28912e03a84f
Mehedi Hasan <hasibulislam2132@gmail.com>
2024-02-19 22:15:04 +0600

    modified the next button function

--- script.js
+++ script.js
@@ -1,26 +1,5 @@
 console.log('connected')
 
-// function newFunction() {
-//   let couponCodes = document.getElementById('NEW15')
-//   let exactCoupon = couponCodes.innerText
-//   let Coupon = document.getElementById('applyButton');
-//   const couponInput = document.getElementById('couponInput');
-//   const couponCode = couponInput.value;
-//   Coupon = couponCode;
-
-
-
-//   if (Coupon === exactCoupon) {
-//     let couponInputField = document.getElementById('couponField')
-//     couponInputField = couponInputField.classList.add('hidden')
-//   }
-//   else {
-//     alert('Your Coupon is Invalid!');
-//   }
-
-// }
-
-console.log('connected');
 
 document.getElementById('goAndBuyBtn').addEventListener('click', function () {
   const divToView = document.getElementById('buyYourTicket');
@@ -114,11 +93,35 @@ setSeatId();
 
 function checkInput() {
   const inputField = document.getElementById('phoneInput');
+  const nameInput = document.getElementById('nameInput');
   const nextButton = document.getElementById('nextButton');
+  const isNameNotEmpty = nameInput.value.trim() !== '';
 
-  if (!isNaN(parseInt(inputField.value))) {
+  if (!isNaN(parseInt(inputField.value)) && isNameNotEmpty) {
     nextButton.removeAttribute('disabled');
   } else {
     nextButton.setAttribute('disabled', 'true');
   }
+}
+
+
+
+function newFunction() {
+  let couponCodes = document.getElementById('NEW15')
+  let exactCoupon = couponCodes.innerText
+  let Coupon = document.getElementById('applyButton');
+  const couponInput = document.getElementById('couponInput');
+  const couponCode = couponInput.value;
+  Coupon = couponCode;
+
+
+
+  if (Coupon === exactCoupon) {
+    let couponInputField = document.getElementById('couponField')
+    couponInputField = couponInputField.classList.add('hidden')
+  }
+  else {
+    alert('Your Coupon is Invalid!');
+  }
+
 }
